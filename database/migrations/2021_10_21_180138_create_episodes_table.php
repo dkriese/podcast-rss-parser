3641ee43f997dbe0ff7794da18a33b156bd9a401
Diana K <dkriese@gmail.com>
2021-10-21 12:30:05 -0600

    adding migrations and model classes for Podcast and Episode

---
+++ database/migrations/2021_10_21_180138_create_episodes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEpisodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('podcast_id');
+            $table->text('title');
+            $table->text('description');
+            $table->text('audio_url');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('episodes');
+    }
+}
